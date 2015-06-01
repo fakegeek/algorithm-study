6a106f26a5ed76be2fc714b51b88e89fdda2b6c4
Ruins He <lyhypacm@gmail.com>
2015-06-01 08:49:59 +0800

    setup mac os bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,13 +5,16 @@
 set -eux
 
 cd build-tools/bazel/
-sudo apt-get update -qq
-sudo apt-get install -y protobuf-compiler libarchive-dev netcat-traditional
-sudo update-alternatives --set nc /bin/nc.traditional
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export JAVA_OPTS="-Xmx3000m"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  brew install protobuf libarchive
+else
+  sudo apt-get update -qq
+  sudo apt-get install -y protobuf-compiler libarchive-dev netcat-traditional
+  sudo update-alternatives --set nc /bin/nc.traditional
+  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+  export JAVA_OPTS="-Xmx3000m"
+fi
 ./compile.sh || return $?
 cd ../..
 ln -s build-tools/bazel/tools .
 ln -s build-tools/bazel/third_party .
-./run_all_tests.sh || return $?
