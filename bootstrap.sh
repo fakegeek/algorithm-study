c760b6c4ce978746e9d4bd2da8523415abfe59d9
Ruins He <lyhypacm@gmail.com>
2015-06-01 08:55:43 +0800

    fix CI build env.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,6 @@ else
   sudo apt-get update -qq
   sudo apt-get install -y protobuf-compiler libarchive-dev netcat-traditional
   sudo update-alternatives --set nc /bin/nc.traditional
-  export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-  export JAVA_OPTS="-Xmx3000m"
 fi
 ./compile.sh || return $?
 cd ../..
